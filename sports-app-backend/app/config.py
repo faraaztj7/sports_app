97e2cb3312b551fb89ecf24d754acc8a697023ec
faraaztawheed7 <87644472+faraaztawheed7@users.noreply.github.com>
2024-11-11 16:27:03 +0530

    Final Changes

--- sports-app-backend/app/config.py
+++ sports-app-backend/app/config.py
@@ -8,8 +8,3 @@ class Config:
         'sqlite:///' + os.path.join(basedir, 'app.db')  # Default DB
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     RESTX_MASK_SWAGGER = False  # To display all fields in Swagger
-
-class TestingConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # In-memory database for testing
-    TESTING = True  # Enable testing mode
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
